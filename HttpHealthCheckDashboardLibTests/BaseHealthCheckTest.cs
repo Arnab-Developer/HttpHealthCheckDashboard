3be105bb9834039e46cb5c7cbb98aca6223d9250
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-06-05 16:37:03 +0530

    wip

--- HttpHealthCheckDashboardLibTests/BaseHealthCheckTest.cs
+++ HttpHealthCheckDashboardLibTests/BaseHealthCheckTest.cs
@@ -8,26 +8,34 @@ using System.Linq;
 using System.Threading;
 using Xunit;
 
-namespace HttpHealthCheckDashboardLibTests.HealthChecks
+namespace HttpHealthCheckDashboardLibTests
 {
-    public class HealthCheckTest
+    public class BaseHealthCheckTest
     {
-        [Fact]
-        public void Can_CheckHealth_ReturnHealthy()
+        private readonly IEnumerable<ApiDetail> _urlDetails;
+        private readonly Mock<ICommonHealthCheck> _commonHealthCheckMock;
+
+        public BaseHealthCheckTest()
         {
-            IEnumerable<ApiDetail> urlDetails = new List<ApiDetail>()
+            _urlDetails = new List<ApiDetail>()
             {
                 new ApiDetail("api1", "url1", new ApiCredential("user1", "pass1"), true),
                 new ApiDetail("api2", "url2", new ApiCredential("user2", "pass2"), true),
                 new ApiDetail("Blog", "url3", new ApiCredential("user3", "pass3"), true),
                 new ApiDetail("api4", "url4", new ApiCredential("user4", "pass4"), true)
             };
-            Mock<ICommonHealthCheck> commonHealthCheckMock = new();
-            commonHealthCheckMock
-                .Setup(s => s.IsApiHealthy(urlDetails.ElementAt(2)))
+            _commonHealthCheckMock = new Mock<ICommonHealthCheck>();
+        }
+
+        [Fact]
+        public void Can_CheckHealth_ReturnHealthy()
+        {
+            _commonHealthCheckMock
+                .Setup(s => s.IsApiHealthy(_urlDetails.ElementAt(2)))
                 .Returns(true);
+
             Microsoft.Extensions.Diagnostics.HealthChecks.IHealthCheck blogHealthCheck
-                = new BlogHealthCheck(urlDetails, commonHealthCheckMock.Object);
+                = new BlogHealthCheck(_urlDetails, _commonHealthCheckMock.Object);
 
             HealthCheckResult healthCheckResult = blogHealthCheck.CheckHealthAsync(
                 new HealthCheckContext(), new CancellationToken()).Result;
@@ -38,19 +46,12 @@ namespace HttpHealthCheckDashboardLibTests.HealthChecks
         [Fact]
         public void Can_CheckHealth_ReturnUnHealthy()
         {
-            IEnumerable<ApiDetail> urlDetails = new List<ApiDetail>()
-            {
-                new ApiDetail("api1", "url1", new ApiCredential("user1", "pass1"), true),
-                new ApiDetail("api2", "url2", new ApiCredential("user2", "pass2"), true),
-                new ApiDetail("Blog", "url3", new ApiCredential("user3", "pass3"), true),
-                new ApiDetail("api4", "url4", new ApiCredential("user4", "pass4"), true)
-            };
-            Mock<ICommonHealthCheck> commonHealthCheckMock = new();
-            commonHealthCheckMock
-                .Setup(s => s.IsApiHealthy(urlDetails.ElementAt(2)))
+            _commonHealthCheckMock
+                .Setup(s => s.IsApiHealthy(_urlDetails.ElementAt(2)))
                 .Returns(false);
+
             Microsoft.Extensions.Diagnostics.HealthChecks.IHealthCheck blogHealthCheck
-                = new BlogHealthCheck(urlDetails, commonHealthCheckMock.Object);
+                = new BlogHealthCheck(_urlDetails, _commonHealthCheckMock.Object);
 
             HealthCheckResult healthCheckResult = blogHealthCheck.CheckHealthAsync(
                 new HealthCheckContext(), new CancellationToken()).Result;
