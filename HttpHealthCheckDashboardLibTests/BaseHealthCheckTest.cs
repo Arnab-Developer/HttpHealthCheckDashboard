4ae811b51c38506ecbea2c3ab789dc8f75ec6742
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-06-10 14:50:07 +0530

    wip

--- HttpHealthCheckDashboardLibTests/BaseHealthCheckTest.cs
+++ HttpHealthCheckDashboardLibTests/BaseHealthCheckTest.cs
@@ -91,5 +91,32 @@ namespace HttpHealthCheckDashboardLibTests
 
             Assert.Equal(HealthCheckResult.Unhealthy(), healthCheckResult);
         }
+
+        [Fact]
+        public void Can_CheckHealth_ReturnUnHealthyIfInvalidMatch()
+        {
+            IEnumerable<ApiDetail> urlDetails = new List<ApiDetail>()
+            {
+                new ApiDetail("api1", "url1", new ApiCredential("user1", "pass1"), true),
+                new ApiDetail("api2", "url2", new ApiCredential("user2", "pass2"), true),
+                new ApiDetail("Test", "url3", new ApiCredential("user3", "pass3"), false),
+                new ApiDetail("api4", "url4", new ApiCredential("user4", "pass4"), true)
+            };
+
+            Mock<ICommonHealthCheck> commonHealthCheckMock = new();
+            TestCustomMatchHealthCheck testCustomMatchHealthCheck = new(urlDetails, commonHealthCheckMock.Object);
+
+            commonHealthCheckMock
+                .Setup(s => s.IsApiHealthy(null))
+                .Returns(false);
+
+            Microsoft.Extensions.Diagnostics.HealthChecks.IHealthCheck healthCheck
+                = testCustomMatchHealthCheck;
+
+            HealthCheckResult healthCheckResult = healthCheck.CheckHealthAsync(
+                new HealthCheckContext(), new CancellationToken()).Result;
+
+            Assert.Equal(HealthCheckResult.Unhealthy(), healthCheckResult);
+        }
     }
 }
---
+++ HttpHealthCheckDashboardLibTests/TestCustomMatchHealthCheck.cs
@@ -0,0 +1,22 @@
+﻿using ArnabDeveloper.HttpHealthCheck;
+using HttpHealthCheckDashboardLib;
+using System;
+using System.Collections.Generic;
+
+namespace HttpHealthCheckDashboardLibTests
+{
+    public class TestCustomMatchHealthCheck : BaseHealthCheck
+    {
+        public TestCustomMatchHealthCheck(IEnumerable<ApiDetail> urlDetails, ICommonHealthCheck commonHealthCheck)
+            : base(urlDetails, commonHealthCheck)
+        {
+        }
+
+        protected override Predicate<ApiDetail> GetMatch()
+        {
+            int indexOfHealthCheck = GetType().Name.IndexOf("CustomMatchHealthCheck");
+            string apiNameToTest = GetType().Name.Substring(0, indexOfHealthCheck);
+            return new Predicate<ApiDetail>(u => u.Name == apiNameToTest && u.IsEnable);
+        }
+    }
+}
