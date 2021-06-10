49fc2feb19b5d878b3cc89183f658fec1710772a
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-06-10 14:23:20 +0530

    wip

--- HttpHealthCheckDashboardLib/BaseHealthCheck.cs
+++ HttpHealthCheckDashboardLib/BaseHealthCheck.cs
@@ -1,5 +1,6 @@
 ﻿using ArnabDeveloper.HttpHealthCheck;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -23,12 +24,19 @@ namespace HttpHealthCheckDashboardLib
         Task<HealthCheckResult> Microsoft.Extensions.Diagnostics.HealthChecks.IHealthCheck.CheckHealthAsync(
             HealthCheckContext context, CancellationToken cancellationToken)
         {
-            string apiNameToTest = GetType().Name.Substring(0, GetType().Name.IndexOf("HealthCheck"));
-            ApiDetail? apiDetail = _urlDetails.ToList().Find(u => u.Name == apiNameToTest && u.IsEnable);
+            Predicate<ApiDetail> match = GetMatch();
+            ApiDetail? apiDetail = _urlDetails.ToList().Find(match);
 
             return _commonHealthCheck.IsApiHealthy(apiDetail)
                 ? Task.FromResult(HealthCheckResult.Healthy())
                 : Task.FromResult(HealthCheckResult.Unhealthy());
+        }
+
+        protected virtual Predicate<ApiDetail> GetMatch()
+        {
+            int indexOfHealthCheck = GetType().Name.IndexOf("HealthCheck");
+            string apiNameToTest = GetType().Name.Substring(0, indexOfHealthCheck);
+            return new Predicate<ApiDetail>(u => u.Name == apiNameToTest && u.IsEnable);
         }
     }
 }
