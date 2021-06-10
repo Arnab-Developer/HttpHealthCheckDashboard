d09f6fc64a9cced4f4be6125d5541661673a013c
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-06-10 14:29:58 +0530

    wip

--- HttpHealthCheckDashboardLibTests/BaseHealthCheckTest.cs
+++ HttpHealthCheckDashboardLibTests/BaseHealthCheckTest.cs
@@ -18,19 +18,19 @@ namespace HttpHealthCheckDashboardLibTests
             {
                 new ApiDetail("api1", "url1", new ApiCredential("user1", "pass1"), true),
                 new ApiDetail("api2", "url2", new ApiCredential("user2", "pass2"), true),
-                new ApiDetail("Base", "url3", new ApiCredential("user3", "pass3"), true),
+                new ApiDetail("Test", "url3", new ApiCredential("user3", "pass3"), true),
                 new ApiDetail("api4", "url4", new ApiCredential("user4", "pass4"), true)
             };
 
             Mock<ICommonHealthCheck> commonHealthCheckMock = new();
-            Mock<BaseHealthCheck> baseHealthCheckMock = new(urlDetails, commonHealthCheckMock.Object);
+            TestHealthCheck testHealthCheck = new(urlDetails, commonHealthCheckMock.Object);
 
             commonHealthCheckMock
                 .Setup(s => s.IsApiHealthy(urlDetails.ElementAt(2)))
                 .Returns(true);
 
             Microsoft.Extensions.Diagnostics.HealthChecks.IHealthCheck healthCheck
-                = baseHealthCheckMock.Object;
+                = testHealthCheck;
 
             HealthCheckResult healthCheckResult = healthCheck.CheckHealthAsync(
                 new HealthCheckContext(), new CancellationToken()).Result;
@@ -45,19 +45,19 @@ namespace HttpHealthCheckDashboardLibTests
             {
                 new ApiDetail("api1", "url1", new ApiCredential("user1", "pass1"), true),
                 new ApiDetail("api2", "url2", new ApiCredential("user2", "pass2"), true),
-                new ApiDetail("Base", "url3", new ApiCredential("user3", "pass3"), true),
+                new ApiDetail("Test", "url3", new ApiCredential("user3", "pass3"), true),
                 new ApiDetail("api4", "url4", new ApiCredential("user4", "pass4"), true)
             };
 
             Mock<ICommonHealthCheck> commonHealthCheckMock = new();
-            Mock<BaseHealthCheck> baseHealthCheckMock = new(urlDetails, commonHealthCheckMock.Object);
+            TestHealthCheck testHealthCheck = new(urlDetails, commonHealthCheckMock.Object);
 
             commonHealthCheckMock
                 .Setup(s => s.IsApiHealthy(urlDetails.ElementAt(2)))
                 .Returns(false);
 
             Microsoft.Extensions.Diagnostics.HealthChecks.IHealthCheck healthCheck
-                = baseHealthCheckMock.Object;
+                = testHealthCheck;
 
             HealthCheckResult healthCheckResult = healthCheck.CheckHealthAsync(
                 new HealthCheckContext(), new CancellationToken()).Result;
@@ -72,19 +72,19 @@ namespace HttpHealthCheckDashboardLibTests
             {
                 new ApiDetail("api1", "url1", new ApiCredential("user1", "pass1"), true),
                 new ApiDetail("api2", "url2", new ApiCredential("user2", "pass2"), true),
-                new ApiDetail("Base", "url3", new ApiCredential("user3", "pass3"), false),
+                new ApiDetail("Test", "url3", new ApiCredential("user3", "pass3"), false),
                 new ApiDetail("api4", "url4", new ApiCredential("user4", "pass4"), true)
             };
 
             Mock<ICommonHealthCheck> commonHealthCheckMock = new();
-            Mock<BaseHealthCheck> baseHealthCheckMock = new(urlDetails, commonHealthCheckMock.Object);
+            TestHealthCheck testHealthCheck = new(urlDetails, commonHealthCheckMock.Object);
 
             commonHealthCheckMock
                 .Setup(s => s.IsApiHealthy(null))
                 .Returns(false);
 
             Microsoft.Extensions.Diagnostics.HealthChecks.IHealthCheck healthCheck
-                = baseHealthCheckMock.Object;
+                = testHealthCheck;
 
             HealthCheckResult healthCheckResult = healthCheck.CheckHealthAsync(
                 new HealthCheckContext(), new CancellationToken()).Result;
---
+++ HttpHealthCheckDashboardLibTests/TestHealthCheck.cs
@@ -0,0 +1,14 @@
+﻿using ArnabDeveloper.HttpHealthCheck;
+using HttpHealthCheckDashboardLib;
+using System.Collections.Generic;
+
+namespace HttpHealthCheckDashboardLibTests
+{
+    public class TestHealthCheck : BaseHealthCheck
+    {
+        public TestHealthCheck(IEnumerable<ApiDetail> urlDetails, ICommonHealthCheck commonHealthCheck)
+            : base(urlDetails, commonHealthCheck)
+        {
+        }
+    }
+}
