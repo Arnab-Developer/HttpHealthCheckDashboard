ab1bb80ab751203f233ba98ed89a19414fb4b8aa
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-06-11 14:49:04 +0530

    Remove url readming from config because now it will be done by the lib

--- HttpHealthCheckDashboard/Startup.cs
+++ HttpHealthCheckDashboard/Startup.cs
@@ -1,13 +1,10 @@
 using Arc.HttpHealthCheckDashboard.DI;
-using ArnabDeveloper.HttpHealthCheck;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace HttpHealthCheckDashboard
 {
@@ -25,27 +22,7 @@ namespace HttpHealthCheckDashboard
         public void ConfigureServices(IServiceCollection services)
         {
             services
-                .AddHttpHealthCheckDashboard()
-                .AddTransient(options =>
-                {
-                    IEnumerable<IConfigurationSection> sections = Configuration.GetSection("ApiDetails").GetChildren();
-                    List<ApiDetail> urlDetails = new();
-                    for (var counter = 0; counter < sections.Count(); counter++)
-                    {
-                        urlDetails.Add(new ApiDetail
-                        (
-                            Configuration.GetValue<string>($"ApiDetails:{counter}:Name"),
-                            Configuration.GetValue<string>($"ApiDetails:{counter}:Url"),
-                            new ApiCredential
-                            (
-                                Configuration.GetValue<string>($"ApiDetails:{counter}:Credential:UserName"),
-                                Configuration.GetValue<string>($"ApiDetails:{counter}:Credential:Password")
-                            ),
-                            Configuration.GetValue<bool>($"ApiDetails:{counter}:IsEnable")
-                        ));
-                    }
-                    return urlDetails.AsEnumerable();
-                })
+                .AddHttpHealthCheckDashboard(Configuration)
                 .AddHealthChecksUrls();
 
             services
